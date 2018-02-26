303633e99fde2f642d7df6c5f19571bb7426b9b7
Camilleryr <camiller.yr@gmail.com>
2018-02-26 11:20:30 -0800

    removed migrations from git

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,4 +2,9 @@
 
 $("#movieSearch__button").click(evt => {
     const userSearchString = $("#movieSearch").val()
+
+    $.ajax({
+        method: "GET",
+        url: `https://api.themoviedb.org/3/search/movie?api_key=0816e8438e2384e0b8907a88cda3ee2b&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
+    }).then(response => console.log(response))
 });
